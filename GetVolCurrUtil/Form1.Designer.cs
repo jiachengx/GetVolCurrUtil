8dec90a34044923d5f334bac499e997eb8e4dbfc
JC Hsu <cchhsu.hsu@gmail.com>
2019-04-01 00:12:04 +0800

    Add files via upload

---
+++ GetVolCurrUtil/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace GetVolCurrUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.voltageTB = new System.Windows.Forms.TextBox();
+            this.currentTB = new System.Windows.Forms.TextBox();
+            this.intervalTB = new System.Windows.Forms.TextBox();
+            this.testTimeTB = new System.Windows.Forms.TextBox();
+            this.remainTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(12, 160);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(174, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Voltage (V) : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current (mA) :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Inteval Time (ms):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Test Time (sec):";
+            // 
+            // voltageTB
+            // 
+            this.voltageTB.Location = new System.Drawing.Point(111, 10);
+            this.voltageTB.Name = "voltageTB";
+            this.voltageTB.Size = new System.Drawing.Size(75, 22);
+            this.voltageTB.TabIndex = 6;
+            // 
+            // currentTB
+            // 
+            this.currentTB.Location = new System.Drawing.Point(111, 38);
+            this.currentTB.Name = "currentTB";
+            this.currentTB.Size = new System.Drawing.Size(75, 22);
+            this.currentTB.TabIndex = 7;
+            // 
+            // intervalTB
+            // 
+            this.intervalTB.Location = new System.Drawing.Point(111, 78);
+            this.intervalTB.Name = "intervalTB";
+            this.intervalTB.Size = new System.Drawing.Size(75, 22);
+            this.intervalTB.TabIndex = 8;
+            // 
+            // testTimeTB
+            // 
+            this.testTimeTB.Location = new System.Drawing.Point(111, 109);
+            this.testTimeTB.Name = "testTimeTB";
+            this.testTimeTB.Size = new System.Drawing.Size(75, 22);
+            this.testTimeTB.TabIndex = 9;
+            // 
+            // remainTime
+            // 
+            this.remainTime.AutoSize = true;
+            this.remainTime.Location = new System.Drawing.Point(12, 135);
+            this.remainTime.Name = "remainTime";
+            this.remainTime.Size = new System.Drawing.Size(8, 12);
+            this.remainTime.TabIndex = 10;
+            this.remainTime.Text = ".";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 196);
+            this.Controls.Add(this.remainTime);
+            this.Controls.Add(this.testTimeTB);
+            this.Controls.Add(this.intervalTB);
+            this.Controls.Add(this.currentTB);
+            this.Controls.Add(this.voltageTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Get Data";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox voltageTB;
+        private System.Windows.Forms.TextBox currentTB;
+        private System.Windows.Forms.TextBox intervalTB;
+        private System.Windows.Forms.TextBox testTimeTB;
+        private System.Windows.Forms.Label remainTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ GetVolCurrUtil/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+#region Rebuild from Orig GetVolCurrTool
+// Add the source from yocto source and then modify export to excel function  
+// Add the voltage and current value and calculate its avg current/ voltage and watt
+// Author: Stephen Hsu
+// Created by 2013.01.08
+#endregion
+
+namespace GetVolCurrUtil
+{
+    public partial class Form1 : Form
+    {
+        private string errmsg = "";
+        private YVoltage voltageSensor;
+        private YModule m = null;
+        private YVoltage voltageSensorDC = null;
+        private YCurrent currentSensor;
+        private YCurrent currentSensorDC = null;
+        private double cSum = 0.0;
+        private double vSum = 0.0;
+        private int timeCount = 0;
+        
+        private List<string> logVolAl;
+        private List<string> logCurrAl;
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (YAPI.RegisterHub("usb", ref this.errmsg) != 0)
+            {
+                MessageBox.Show("RegisterHub error: " + this.errmsg, "Error");
+            }
+            else
+            {
+                this.logCurrAl = new List<string>();
+                this.logVolAl = new List<string>();
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (startBtn.Text.Equals("Start"))
+            {
+                this.remainTime.Text = "";
+                this.logVolAl.Clear();
+                this.logCurrAl.Clear();
+                this.cSum = 0.0;
+                this.vSum = 0.0;
+                if (this.intervalTB.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Please insert interval time.", "Warning");
+                }
+                else
+                {
+                    if (this.testTimeTB.Text.Trim().Equals(""))
+                    {
+                        MessageBox.Show("Please insert test time.", "Warning");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            this.timer1.Interval = Convert.ToInt32(this.intervalTB.Text.Trim());
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Error(Interval Time)");
+                            return;
+                        }
+                        try
+                        {
+                            this.timeCount = Convert.ToInt32(this.testTimeTB.Text.Trim());
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Error(Test Time)");
+                            return;
+                        }
+                        this.voltageSensor = YVoltage.FirstVoltage();
+                        if (this.voltageSensor != null && this.voltageSensor.isOnline())
+                        {
+                            this.m = this.voltageSensor.get_module();
+                            this.voltageSensorDC = YVoltage.FindVoltage(this.m.get_serialNumber() + ".voltage1");
+                            this.m = null;
+                            this.currentSensor = YCurrent.FirstCurrent();
+                            if (this.currentSensor != null && this.currentSensor.isOnline())
+                            {
+                                this.m = this.currentSensor.get_module();
+                                this.currentSensorDC = YCurrent.FindCurrent(this.m.get_serialNumber() + ".current1");
+                                this.m = null;
+                                //this.startBtn.Enabled = false;
+                                this.startBtn.Text = "Stop";
+                                this.timer1.Enabled = true;
+                                this.timer2.Enabled = true;
+                                this.remainTime.Visible = true;
+                                //this.stopBtn.Enabled = true;
+                                this.intervalTB.Enabled = false;
+                                this.testTimeTB.Enabled = false;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Current module not connected.", "Error");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Voltage module not connected.", "Error");
+                        }
+                    }
+                }
+            }
+            else 
+            {
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.remainTime.Visible = false;
+                this.intervalTB.Enabled = true;
+                this.testTimeTB.Enabled = true;
+                this.startBtn.Text = "Start";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+           
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.timeCount == 0)
+            {
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.remainTime.Visible = false;
+                this.writeCSVFile(this.logVolAl, this.logCurrAl);
+            }
+            else
+            {
+               
+                this.voltageTB.Text = this.voltageSensorDC.get_currentRawValue().ToString("F2");
+                this.logVolAl.Add(this.voltageTB.Text);
+                this.currentTB.Text = this.currentSensorDC.get_currentRawValue().ToString();
+                this.logCurrAl.Add(this.currentTB.Text);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.timeCount--;
+            this.remainTime.Text = this.timeCount.ToString();
+        }
+        private void writeCSVFile(List<string> vol, List<string> curr)
+        {
+            this.Text = "Writing to Log File ....";
+            DateTime now = DateTime.Now;
+            string text = "";
+            text += now.Year.ToString();
+            if (now.Month < 10)
+            {
+                text += "0";
+            }
+            text += now.Month.ToString();
+            if (now.Day < 10)
+            {
+                text += "0";
+            }
+            text = text + now.Day.ToString() + "-";
+            if (now.Hour < 10)
+            {
+                text += "0";
+            }
+            text += now.Hour.ToString();
+            if (now.Minute < 10)
+            {
+                text += "0";
+            }
+            text += now.Minute.ToString();
+            if (now.Second < 10)
+            {
+                text += "0";
+            }
+            text += now.Second.ToString();
+
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Log");
+            using (StreamWriter streamWriter = new StreamWriter(Directory.GetCurrentDirectory() + "\\Log\\" + "Benchmark_" + text + ".csv"))
+            {
+                
+                streamWriter.WriteLine("Voltage,Current");
+                for (int i = 0; i < vol.Count; i++)
+                {
+                    streamWriter.WriteLine(vol[i] + "," + curr[i]);
+                    cSum += Convert.ToDouble(curr[i]);
+                    vSum += Convert.ToDouble(vol[i]);
+                }
+                
+                // add avg. voltage/ current / watt
+                streamWriter.WriteLine(string.Format("\nAvg. Voltage, {0:F2}", vSum / vol.Count));
+                streamWriter.WriteLine(string.Format("Avg. Current, {0:F2}", cSum / curr.Count));
+                streamWriter.WriteLine(string.Format("Avg. Watt, {0:F2}", vSum * cSum / (vol.Count * curr.Count)));
+            }
+            this.Text = "Get Data";
+            this.startBtn.Text = "Start";
+            this.intervalTB.Enabled = true;
+            this.testTimeTB.Enabled = true;
+        }
+    }
+
+}
